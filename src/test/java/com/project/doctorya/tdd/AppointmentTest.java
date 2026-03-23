c1ce41e9729aefd580c78f0134728bcfdd13607b
ingcarolinasanchezg <ingcarolinasanchezg@gmail.com>
2026-03-23 11:04:52 -0500

    se completa test de ATDD

--- src/test/java/com/project/doctorya/tdd/AppointmentTest.java
+++ src/test/java/com/project/doctorya/tdd/AppointmentTest.java
@@ -12,7 +12,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
 
 import com.project.doctorya.dtos.AppointmentDto;
 import com.project.doctorya.models.Appointment;
